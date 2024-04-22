6437d9cd4b0ccfcbfeaa2e4c9a7ad20a6a5d4b64
LAHAN Happiness Tresor <139039151+Tresor091005@users.noreply.github.com>
2024-04-22 08:21:45 +0100

    Update countdown date

--- main.js
+++ main.js
@@ -7,4 +7,4 @@ for (const link of dom.links) {
     link.addEventListener('mouseleave', () => {ChangePathColor(link.firstChild, "#8385A9")})
 }
 
-runClock(dom.flip, "April 2, 2024", () => { alert('Heureux Jour de Fête') });
+runClock(dom.flip, "October 9, 2024", () => { alert('Heureux Jour de Fête') });
